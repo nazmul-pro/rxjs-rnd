d65234ce6b93397f4676466ae2fded0b7c729691
Shadhin Sumon <shadhinrcrg@gmail.com>
2019-03-01 20:21:05 +0600

    Range

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Rx from "rxjs/Rx";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  ngOnInit() {
+  	var source = Rx.Observable.range(0, 3);
+
+	var subscription = source.subscribe(
+	    function (x) { console.log('Next: ' + x); },
+	    function (err) { console.log('Error: ' + err); },
+	    function () { console.log('Completed'); });
+  }
 }
